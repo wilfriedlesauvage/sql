d0b898431ed87bd55d4015b817cc589f65d25080
wilfriedlesauvage <obistrip@outlook.fr>
2020-05-30 08:58:58 +0200

    maj

--- script.sql
+++ script.sql
@@ -13,58 +13,57 @@ GRANT ALL PRIVILEGES ON mediabank.* TO 'wilfried'@'localhost';
 
 
 --Exo5
-CREATE TABLE images
+CREATE TABLE `images`
 (
-    	id INT PRIMARY KEY AUTO_INCREMENT,
-        name varchar(50),
-    	link varchar(255),
-    	width INT,
-    	height INT,
-    	format varchar(5),
-    	description TEXT,
-    	publicationDate DATE
+    	`id` INT PRIMARY KEY AUTO_INCREMENT,
+        `name` VARCHAR(50),
+    	`link` VARCHAR(255),
+    	`width` INT,
+    	`height` INT,
+    	`format` VARCHAR(5),
+    	`description` TEXT,
+    	`publicationDate` DATE
 );
     	
 
 
-CREATE TABLE videos
+CREATE TABLE `videos`
 (
-    	id INT PRIMARY KEY AUTO_INCREMENT,
-        name varchar(50),
-    	link varchar(255),
-    	duration TIME,
-    	description TEXT,
-    	publicationDate DATE
+    	`id` INT PRIMARY KEY AUTO_INCREMENT,
+        `name` VARCHAR(50),
+    	`link` VARCHAR(255),
+    	`duration` TIME,
+    	`description` TEXT,
+    	`publicationDate` DATE
 );
 
 
-
 --Exo6
-INSERT INTO images (id, name, link, width, height, format, description, publicationDate)
+INSERT INTO `images` (`id`, `name`, `link`, `width`, `height`, `format`, `description`, `publicationDate`)
 VALUES ('1', 'orwellPortrait', 'https://upload.wikimedia.org/wikipedia/commons/7/7e/George_Orwell_press_photo.jpg', '1176', '1596', 'jpg', 'Portrait de l’auteur Geroge Orwell', '2020-05-10'),
 ('2', 'steinbeckPortrait', 'https://live.staticflickr.com/525/18306511889_1c9606e8ea_z.jpg', '640', '476', 'jpg', 'Portrait de l’auteur John Steinbeck', '2020-01-02');
 
-SELECT * FROM `images` 
+SELECT `id`, `name`, `link`, `width`, `height`, `format`, `description`, `publicationDate` FROM `images` 
 
 
-INSERT INTO videos (id, name, link, duration, description, publicationDate)
+INSERT INTO `videos` (`id`, `name`, `link`, `duration`, `description`, `publicationDate`)
 VALUES ('1', '1984_trailer', 'https://www.youtube.com/watch?v=Z4rBDUJTnNU&t=4s', '2 :39', 'Bande annonce du film 1984', '2020-05-15'),
 ('2', 'micemen_trailer', 'https://www.youtube.com/watch?v=2QnM6hsXxNE', '10 :41', 'Bande annonce du film des Souris et des hommes', '2020-01-02');
 
-SELECT * FROM `videos`
+SELECT `id`, `name`, `link`, `duration`, `description`, `publicationDate` FROM `videos` 
 
 
 
 --Exo7
-UPDATE images
-SET publicationDate = '2020-05-27'
-WHERE id = 1
+UPDATE `images`
+SET `publicationDate` = '2020-05-27'
+WHERE `id` = 1
 
 SELECT * FROM `images` WHERE id = 1
 
-UPDATE videos
-SET duration = '1 :41'
-WHERE id = 2
+UPDATE `videos`
+SET `duration` = '1 :41'
+WHERE `id` = 2
 
 SELECT * FROM `videos` WHERE id = 2
 
